188c58cdd2bca2f71f9a157b3ca730ea101ca4b0
md shahbaz alam <alams3510@gmail.com>
2023-01-20 18:35:37 +0530

    updated again cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
 const multer=require("multer");
 const path = require("path");
+const cors=require("cors");
 
 dotenv.config();
 const PORT=process.env.PORT||3001
@@ -30,6 +31,7 @@ const connectDB=async()=>{
 //middleware
 app.use("/images",express.static(path.join(__dirname,"public/images")))
 app.use(express.json());
+app.use(cors())
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
