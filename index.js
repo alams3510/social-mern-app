802e132b856a8f7c9a5d49968b91bdd0ec81152c
md shahbaz alam <alams3510@gmail.com>
2023-01-20 19:59:47 +0530

    updated

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("/api/posts", postsRoute);
 //serving frontend to backend
 app.use(express.static(path.join(__dirname,"./frontend/build")))
 app.use("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"./frontend/build/index.html"))
+  res.sendFile(path.join(__dirname,"./frontend/build", "index.html"))
 })
 
 //uploading an image
