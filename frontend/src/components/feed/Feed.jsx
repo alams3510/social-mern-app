fab0490e1a3f53f672730ed7afbfc64a6657b58e
md shahbaz alam <alams3510@gmail.com>
2023-01-23 23:44:33 +0530

    updated

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import axios from 'axios';
+import {axiosInstance} from '../../config';
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
@@ -13,8 +13,8 @@ const Feed = ({username}) => {
     useEffect(()=>{
      const fetchPosts=async()=>{
      const res=username
-     ?await axios.get("/posts/profile/"+username)
-     : await axios.get(`posts/timeline/${user._id}`);
+     ?await axiosInstance.get("/posts/profile/"+username)
+     : await axiosInstance.get(`posts/timeline/${user._id}`);
      setPosts(res.data);
      res.data.sort((p1,p2)=>{
       return new Date(p2.createdAt)- new Date(p1.createdAt);
